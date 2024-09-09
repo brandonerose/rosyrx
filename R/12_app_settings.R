8b340506ddbc77e836d50ff473e7b6be81383d58
brandonerose <brandon.ed.rose@gmail.com>
2024-09-09 13:14:34 -0400

    dev

--- R/12_app_settings.R
+++ R/12_app_settings.R
@@ -17,7 +17,7 @@ golem_add_external_resources <- function(...) {
       path = app_sys("app/www"),
       app_title = .packageName
     ),
-    includeCSS(system.file(package="table1", "table1_defaults_1.0/table1_defaults.css")),
+    # includeCSS(system.file(package="table1", "table1_defaults_1.0/table1_defaults.css")),
     ...
   )
 }
@@ -54,20 +54,28 @@ get_golem_config <- function(
     use_parent = use_parent
   )
 }
+#' @title run_RosyRx
 #' @param ... arguments to pass to golem_opts.
 #' See `?golem::get_golem_options` for more details.
 #' @inheritParams shiny::shinyApp
-#'
 #' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
-run_app <- function(
+run_RosyRx <- function(
     onStart = NULL,
-    options = list(),
     enableBookmarking = NULL,
     uiPattern = "/",
+    options = list(
+      launch.browser = T
+    ),
     ...
 ) {
+  if(golem::app_dev()){
+    message("Is Dev! Make sure to load `Library('rstudioapi')`")
+    options<- options(
+      shiny.launch.browser = .rs.invokeShinyPaneViewer
+    )
+  }
   with_golem_options(
     app = shinyApp(
       ui = app_ui,
